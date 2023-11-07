b30546beefd9910308f4e7e4450098e409335889
armoredchimp <mattkasmir@gmail.com>
2023-11-06 21:43:39 -0800

    11/6

--- 1-node-farm/starter/other.js
+++
@@ -1,9 +0,0 @@
-let xhr = new XMLHttpRequest();
-xhr.open(
-  "POST",
-  "arn:aws:execute-api:us-west-2:915385912796:uv990efvtf/*/POST/compare-yourself"
-);
-xhr.onreadystatechange = function (event) {
-  console.log(event.target.response);
-};
-xhr.send();
