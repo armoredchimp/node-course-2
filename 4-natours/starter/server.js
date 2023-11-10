1b942ce2d117f30ca73a06d83d8e5d51348da722
armoredchimp <mattkasmir@gmail.com>
2023-11-10 11:55:16 -0800

    11/10

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,12 @@
+require('dotenv').config();
+
 const app = require('./app');
 
-const port = 6000;
+const port = process.env.PORT || 8000;
+
+// console.log(app.get('env')) //development
+
+// console.log(process.env);
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
