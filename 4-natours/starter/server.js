bbe1435210c58792920c3658f098452fd2188b8d
armoredchimp <mattkasmir@gmail.com>
2023-11-15 12:39:14 -0800

    11/15

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,12 +1,22 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = require('./app');
 
 const port = process.env.PORT || 8000;
 
-// console.log(app.get('env')) //development
+const db = process.env.DB.replace('<password>', process.env.DB_PASSWORD);
 
-// console.log(process.env);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('Moo! DB connected');
+  });
 
 app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
