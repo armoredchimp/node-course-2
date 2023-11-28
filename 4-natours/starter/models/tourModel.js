8cb7aa9c31d2193d87f7dedd060d4d2b0ba672d9
armoredchimp <mattkasmir@gmail.com>
2023-11-27 18:04:06 -0800

    11/20

--- 4-natours/starter/models/tourModel.js
+++ 4-natours/starter/models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Tour requires a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'Tour requires a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'Tour requires a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'Tour requires a difficulty'],
+  },
+  ratingAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'Tour requires a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'Tour requires a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'Tour requires a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
