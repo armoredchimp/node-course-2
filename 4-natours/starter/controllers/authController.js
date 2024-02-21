9b6ead3c1670d3247f580c83fdfeed67ecc1f4d3
armoredchimp <mattkasmir@gmail.com>
2024-02-20 19:31:34 -0800

    2/20

--- 4-natours/starter/controllers/authController.js
+++ 4-natours/starter/controllers/authController.js
@@ -49,3 +49,23 @@ exports.login = catchAsync(async (req, res, next) => {
     token,
   });
 });
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //Get token
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(new AppError('You are not logged in', 401));
+  }
+  //Verify token
+
+  // Check if user still exists
+
+  // Check if user changed password after JWT was issued
+  next();
+});
